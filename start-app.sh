61093f0b6f5daa47a776c3874a201a3ce200cc19
Pedro Silva <zepedro.cs@gmail.com>
2016-02-27 12:49:44 +0000

    adds devise gem. updates docker scripts

--- start-app.sh
+++ start-app.sh
@@ -2,6 +2,5 @@
 
 docker-compose up > /dev/null &
 sleep 2
-docker-compose run -e “RAILS_ENVIRONMENT=development” web bundle install
 docker-compose run -e “RAILS_ENVIRONMENT=development” web rake db:migrate
 docker-compose run -e “RAILS_ENVIRONMENT=development” web rake faker:articles
