ffdf8b9ab36beb3064f58e44e23bccaf337706a9
Pedro Silva <zepedro.cs@gmail.com>
2016-03-01 23:45:53 +0000

    remove login on homepage

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: [:show, :new, :edit, :create, :update, :destroy]
   before_action :set_article, only: [:show, :edit, :update, :destroy]
 
   # GET /articles
